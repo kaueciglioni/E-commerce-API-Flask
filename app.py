1f96dc7232e56911f147b13be928fd4cb433b6dc
Kaue Negrini Ciglioni <kaueciglioni@hotmail.com>
2024-05-24 14:51:00 -0300

    Integração ao banco de dados para realizar o CRUD nos produtos do e-commerce.

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 #importando o FLASK
 
-from flask import Flask, request
+from flask import Flask, request, jsonify
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -19,10 +19,30 @@ class Product(db.Model):
 @app.route('/api/products/add', methods=["POST"])
 def add_product():
     data = request.json
-    return data
+    if 'name' in data and 'price' in data:
+        product = Product(name=data["name"], price=data["price"], description=data.get("description", ""))
+        db.session.add(product)
+        db.session.commit()
+        return jsonify({"message": "Produt added successfully"})
+    return jsonify({"message": "Invalid product data"}), 400
 
+
+@app.route('/api/products/delete/<int:product_id>', methods=["DELETE"])
+def delete_product(product_id):
+    
+    # RECUPERAR O PRODUTO DA BASE DE DADOS
+    # VERIFICAR SE O PRODUTO É VÁLIDO
+    # SE EXISTE, APAGAR DA BASE DE DADOS
+    # SE NÃO EXISTE, RETORNAR NOT FOUND
+    
+    product = Product.query.get(product_id)
+    if product:
+        db.session.delete(product)
+        db.session.commit()
+        return jsonify({"message": "Produt delete successfully"})
+    return jsonify({"message": "Product not found"}), 404
+        
 #definir um rota raiz (Pagina inicial) e a função que sera executada ao requisitar
-
 @app.route('/teste')
 def hello_word():
     return 'Hello Word'
